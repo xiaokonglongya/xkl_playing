b4b6453ee588ffa5e13ece99702c4507d8756ab2
满目山河 <46342554+xiaokonglongya@users.noreply.github.com>
2022-03-27 23:45:48 +0800

    add :airplane:  增加取消CSDN禁止复制代码片段

---
+++ TMJavaScript/cancelCSDNDIsableCopy/index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         CSDN取消禁止复制
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://blog.csdn.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelector('#content_views').id=''
+    // Your code here...
+})();
